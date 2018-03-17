55c58facd72642c2de19f135a9d6b22ba3af350a
Jihye Eom <>
2018-03-18 02:46:20 +0900

    initial project checkin

---
+++ Xamarin_Slider/Xamarin_Slider/GreetPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms.Xaml;
+
+using Xamarin.Forms;
+
+namespace Xamarin_Slider
+{
+    //Skip XAML compilation
+    [XamlCompilation(XamlCompilationOptions.Skip)]
+    public partial class GreetPage : ContentPage
+    {
+        
+        public GreetPage()
+        {
+            InitializeComponent();
+            slider.Value = 0.5;
+        }
+
+        void Handle_ValueChanged(object sender, Xamarin.Forms.ValueChangedEventArgs e)
+        {
+            label.Text = String.Format("Value is {0:F2}", e.NewValue);
+        }
+
+    }
+}
---
+++ Xamarin_Slider/Xamarin_Slider/Xamarin_SliderPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Xamarin_Slider
+{
+    public partial class Xamarin_SliderPage : ContentPage
+    {
+        public Xamarin_SliderPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
